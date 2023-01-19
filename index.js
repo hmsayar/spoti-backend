dff3acdf3a704b9a492ebe14312d3d3fd43fd358
mert sayar <merttsayar@gmail.com>
2023-01-19 22:34:44 +0300

    c

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 const server = http.createServer(app);
 
 var corsOptions = {
-	origin: [process.env.FRONT_URI],
+	origin: [process.env.FRONT_URI, process.env.REXP],
 	credentials: true,
 };
 
